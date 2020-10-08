5f50e95d8c3c02262268efe9904dbe6b10d4dd99
jordansantosj <67442300+jordansantosj@users.noreply.github.com>
2020-10-08 01:22:18 -0300

    Primeiro Commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+let message: string = "Hello World"
+console.log(message);
+
+let episode: number = 4
+
+console.log("This is episode" + 4)
+episode = episode + 1
+console.log("Next episode is "+ episode)
+
+let favoriteDroid //Any
+favoriteDroid = 'bb-8'
+favoriteDroid = 10
+console.log("My favourite droid is" + favoriteDroid)
---
+++ dist/app.js
@@ -0,0 +1,10 @@
+var message = "Hello World";
+console.log(message);
+var episode = 4;
+console.log("This is episode" + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favoriteDroid; //Any
+favoriteDroid = 'bb-8';
+favoriteDroid = 10;
+console.log("My favourite droid is" + favoriteDroid);
